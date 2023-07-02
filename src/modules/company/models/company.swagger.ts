08f6c0e300c791cccd7ac255fbfce1f3bf3f4506
Abdurahim <89141756+pxls00@users.noreply.github.com>
2023-07-02 23:38:58 +0500

    swagger documentation for company endpoints /auth/companies methods get and post

---
+++ src/modules/company/models/company.swagger.ts
@@ -0,0 +1,54 @@
+/**
+ * @openapi
+ *  components:
+ *    schemas:
+ *      CreateCompanyFields:
+ *        type: object
+ *        required:
+ *          - email
+ *          - name
+ *          - phone_number
+ *          - description
+ *        properties:
+ *          email:
+ *            type: string
+ *            default: balenciaga@example.com
+ *          name:
+ *            type: string
+ *            default: Balenciaga
+ *          phone_number:
+ *            type: string
+ *            default: (00) 000-00-00
+ *          description:
+ *            type: string
+ *            default: "The most popular dress store"
+ *      CreateUserResponse:
+ *        type: object
+ *        properties:
+ *          _id:
+ *            type: string
+ *          email:
+ *            type: string
+ *          name:
+ *            type: string
+ *          phone_number:
+ *            type: string
+ *          description:
+ *            type: string
+ *          logo_img:
+ *            type: string
+ *          created_at:
+ *            type: Date
+ *          orders_count:
+ *            type: number
+ *          rate:
+ *            type: array
+ *          rate_base:
+ *            type: number
+ *          followers:
+ *            type: array
+ *          followers_count:
+ *            type: number
+ *          banner_img:
+ *            type: string
+ */
