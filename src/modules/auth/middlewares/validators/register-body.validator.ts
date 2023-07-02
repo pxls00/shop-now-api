a0c19b434ec04b11bec0b23adb8ebd251d3f6b76
Abdurahim <89141756+pxls00@users.noreply.github.com>
2023-07-02 23:40:09 +0500

    refactore code validators and added hooks to mongoose user model

--- src/modules/auth/middlewares/validators/register-body.validator.ts
+++ src/modules/auth/middlewares/validators/register-body.validator.ts
@@ -4,11 +4,11 @@ function checkBodyRequest(): ValidationChain[] {
   return [
     check(
       'name',
-      `Name of the user must be more 5 and lesser 30 characters`
+      `Name of the user must be more 5 and lesser than 30 characters`
     ).isLength({ min: 5, max: 30 }),
     check(
       'password',
-      'Password of user should be more 8 and lesser 16 characters'
+      'Password of user should be more 8 and lesser than 16 characters'
     ).isLength({ min: 8, max: 16 }),
     check('email', 'Email must contain @').isEmail(),
   ]
