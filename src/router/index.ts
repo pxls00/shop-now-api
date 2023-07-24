6be682524b5187e1398beba5faa72bf96df29156
Abdurahim <89141756+pxls00@users.noreply.github.com>
2023-07-24 21:49:07 +0500

    module/filter-option

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,12 @@ import { CompanyRouter } from '../modules/company'
 import { AuthRouter } from '../modules/auth'
 import { UserRouter } from '../modules/user'
 import { CategoryRouter } from '../modules/category'
-import { FilterRouter, FilterColorRouter, FilterOptionRouter, FilterBrandRouter } from '../modules/filters'
+import {
+  FilterRouter,
+  FilterColorRouter,
+  FilterOptionRouter,
+  FilterBrandRouter,
+} from '../modules/filters'
 import config from '../lib/default'
 
 const router = Router()
@@ -14,7 +19,7 @@ router.use(config.apiBaseURL, UserRouter)
 router.use(config.apiBaseURL, CategoryRouter)
 router.use(config.apiBaseURL, FilterRouter)
 router.use(config.apiBaseURL, FilterColorRouter)
-// router.use(config.apiBaseURL, FilterOptionRouter)
+router.use(config.apiBaseURL, FilterOptionRouter)
 router.use(config.apiBaseURL, FilterBrandRouter)
 
 // Swager page
