ae4dfe3bb6d471444dc251ebaff4d45af87fc524
Abdurahim <89141756+pxls00@users.noreply.github.com>
2023-08-01 13:35:58 +0500

    documentation swagger routes product-brand

---
+++ src/modules/product-options/swagger/routes/product-brand-update-delete.swagger.ts
@@ -0,0 +1,48 @@
+/**
+ *  @openapi
+ *  /api/product_brands/{brand_id}:
+ *    patch:
+ *      tags:
+ *      - Product Options
+ *      summary: Edit Product Brand by Id
+ *      parameters:
+ *      - name: brand_id
+ *        in: path
+ *        description: The id of the category for sort brands
+ *        required: true
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/CreateProductBrandFields'
+ *      responses:
+ *        201:
+ *          description: Success
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/ProductBrand'
+ *        409:
+ *          description: Conflict
+ *        400:
+ *          description: Bad Request
+ *        404:
+ *          description: Product Brand not found
+ *    delete:
+ *      tags:
+ *      - Product Options
+ *      summary: Delete Product Brand by Id
+ *      parameters:
+ *      - name: brand_id
+ *        in: path
+ *        description: The id of the category for sort brands
+ *        required: true
+ *      responses:
+ *        201:
+ *          description: Product Brand deleted successfully
+ *        400:
+ *          description: Bad Request
+ *        404:
+ *          description: Product Brand not found
+ */
