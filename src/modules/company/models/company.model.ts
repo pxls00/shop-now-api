2ec15c57c956a62cf546b224a1a3dc52ddb65f05
Abdurahim <89141756+pxls00@users.noreply.github.com>
2023-08-14 18:00:06 +0500

    fix company model population bug

--- src/modules/company/models/company.model.ts
+++ src/modules/company/models/company.model.ts
@@ -6,87 +6,84 @@ import autopopulate from 'mongoose-autopopulate'
 import type { ICompanyDocument } from './company.types'
 // import { IRateItem } from './company.types'
 
-const CompanySchema = new Schema({
-  name: {
-    required: true,
-    type: String,
-  },
-  logo_img: {
-    type: String,
-    default: '',
-  },
-  description: {
-    required: true,
-    type: String,
-  },
-  created_at: {
-    type: Date,
-    default: Date.now,
-  },
-  orders_count: {
-    type: Number,
-    default: 0,
-  },
-  rate: [
-    {
-      user: {
+const CompanySchema = new Schema(
+  {
+    name: {
+      required: true,
+      type: String,
+    },
+    logo_img: {
+      type: String,
+      default: '',
+    },
+    description: {
+      required: true,
+      type: String,
+    },
+    created_at: {
+      type: Date,
+      default: Date.now,
+    },
+    orders_count: {
+      type: Number,
+      default: 0,
+    },
+    rate: [
+      {
+        user: {
+          type: Schema.Types.ObjectId,
+          ref: 'users',
+          required: true,
+          autopopulate: true,
+        },
+        comment: {
+          type: String,
+          default: '',
+        },
+        rate_number: {
+          type: Number,
+          required: true,
+        },
+        created_at: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    rate_base: {
+      type: Number,
+      default: 5.0,
+    },
+    followers: [
+      {
         type: Schema.Types.ObjectId,
         ref: 'users',
         required: true,
         autopopulate: true,
       },
-      comment: {
-        type: String,
-        default: '',
-      },
-      rate_number: {
-        type: Number,
-        required: true,
-      },
-      created_at: {
-        type: Date,
-        default: Date.now,
-      },
+    ],
+    followers_count: {
+      type: Number,
+      default: 0,
     },
-  ],
-  rate_base: {
-    type: Number,
-    default: 5.0,
-  },
-  followers: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'users',
+    banner_img: {
+      type: String,
+      default: '',
+    },
+    email: {
+      type: String,
       required: true,
-      autopopulate: true,
     },
-  ],
-  followers_count: {
-    type: Number,
-    default: 0,
+    phone_number: {
+      type: String,
+      required: true,
+    },
   },
-  banner_img: {
-    type: String,
-    default: '',
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  phone_number: {
-    type: String,
-    required: true,
-  },
-})
-
-CompanySchema.post('find', function (docs: ICompanyDocument[]) {
-  for (const doc of docs) {
-    // doc.followers_count = doc.followers?.length as number
-    doc.followers = undefined
-    // doc.rate_base = getArhimeticMeanNumber(doc.rate as IRateItem[])
-    doc.rate = undefined
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   }
-})
+)
 
 CompanySchema.plugin(autopopulate)
 
