4a8b1d5736234ab35f283a025924fdbbc6d2abf4
Abdurahim <89141756+pxls00@users.noreply.github.com>
2023-08-18 19:23:20 +0500

    module search system for product and company tags both config vars

---
+++ src/modules/saeach-systems/lib/config.ts
@@ -0,0 +1,8 @@
+import type { IConfigModule } from './config.types'
+
+const config: IConfigModule = {
+  moduleRouteCompanySearchURL: '/company_search',
+  moduleRouteProductSearchURL: '/product_search',
+}
+
+export default config
