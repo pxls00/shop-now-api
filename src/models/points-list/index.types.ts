050780c77b693651f4932dfa5e0fd4da48ecb8c0
Abdurahim <89141756+pxls00@users.noreply.github.com>
2023-11-09 19:16:40 +0500

    model points-list

---
+++ src/models/points-list/index.types.ts
@@ -0,0 +1,36 @@
+// Enums
+import type { Document } from 'mongoose'
+
+// Types
+
+// Interfaces
+export interface IPointLocationDegrees {
+  lng: string
+  ltd: string
+}
+
+export interface IPointLocation {
+  city: string
+  district: string
+  orientation: string
+  degrees: IPointLocationDegrees
+}
+
+export interface IPoinWorkTime {
+  work_time: string
+  non_working_days: string
+}
+
+export interface IPointRelatedAdmin {
+  email: string
+  name: string
+}
+export interface IPointItem {
+  location: IPointLocation
+  worktime: IPoinWorkTime
+  created_at: Date
+  // temporarily
+  related_admin: IPointRelatedAdmin
+}
+
+export interface IPointItemDocument extends Document {}
